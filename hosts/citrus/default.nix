5b88790bcf1e5b02bded3f2ad48820d0d04bcca1
Lupo409 <249657796+lupo409@users.noreply.github.com>
2026-02-03 15:39:43 +0900

    fix: use linuxPackages_xanmod_latest

--- hosts/citrus/default.nix
+++ hosts/citrus/default.nix
@@ -14,7 +14,7 @@
     ../common/nix-ld.nix
   ];
 
-  boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linuxKernel.kernels.linux_xanmod_latest;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
   networking.hostName = "Citrus";
 
